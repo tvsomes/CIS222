73072a6ea2e617a4404e964312405fd2e1b271c5
tvsomes <54868385+tvsomes@users.noreply.github.com>
2019-09-09 15:45:32 -0400

    Create Contact.php

---
+++ Contact.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+
+
+<title>Contact Tim Somes</title>
+
+</head>
+<body>
+<?php
+/**
+* contact.php
+*
+* Homework 1
+*
+* @Category Homework
+* @package  Homework 1
+* @author   Tim Somes <tvsomes@hhawkmail.hfcc.edu>
+* @version  2019.09.09
+* @link     https://cislinux.hfcc.edu/~tvsomes/cis222/contact.php/
+*
+ */
+
+
+	h1>Tim Somes</h1>
+<p><?php echo '</p>'; ?>
+   <p>Slack Username: Timothy Somes</p>
+<p><?php echo '</p>'; ?>
+	<p>email: tvsomes@hawkmail.hfcc.edu</p>
+<p><?php echo '</p>'; ?>
+
+</body>
+</html>
